95ec8bd65bb77602de4f8814a774cc946745e244
davidcorteso <d.i.cortes@soton.ac.uk>
2017-10-24 20:51:05 +0100

    Updated animation script

--- animation/animation.py
+++ animation/animation.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import os
 from os import listdir
@@ -14,13 +13,14 @@ mlab.options.offscreen = True
 import scipy.interpolate
 
 
-# # Load Data
+# Load Data
 
 mu0 = 4 * np.pi * 1e-7
 Ms = 0.658e6
 
-base_folder = '../spin_waves_fidimag/OOMMF_periodicDMI_D3e-3_a-w-ratios_Py/omfs_w50/'
-static_state = '../spin_waves_fidimag/OOMMF_periodicDMI_D3e-3_a-w-ratios_Py/InitialMagnetisation_w50_ordered.omf'
+base_folder = '../sim/omfs_w50/'
+static_state = '../sim/InitialMagnetisation_w50_ordered.omf'
+
 
 def key_f(f):
     regex_res = re.search(r'(?<=Magnetization\-)[0-9]+(?=\-)', f).group(0)
@@ -31,7 +31,8 @@ file_list = [_file for _file in listdir(base_folder)
 file_list = sorted(file_list, key=key_f)
 
 
-data0 = pd.read_csv(static_state, comment='#', header=None, delim_whitespace=True)
+data0 = pd.read_csv(static_state, comment='#', header=None,
+                    delim_whitespace=True)
 data0 = data0.as_matrix()
 
 coordinates = data0[:, :3]
@@ -42,21 +43,24 @@ ny = len(np.unique(coordinates[:, 1]))
 mask = coordinates[:, 1] == coordinates[:, 1][nx * int(ny * 0.5)]
 
 
+# -----------------------------------------------------------------------------
+
+
 def plot_i_state(i):
     data = pd.read_csv(os.path.join(base_folder, file_list[i]), comment='#',
-                        header=None, delim_whitespace=True)
+                       header=None, delim_whitespace=True)
     data = data.as_matrix()
 
     data[:, 3:] = (data[:, 3:] - data0[:, 3:]) / Ms
 
     # mz = data[:, 5].reshape(-1, len(np.unique(data[:, 0]))) / Ms
-    
+
     return data
 
 
 # Coordinates and grid
 x, y = np.unique(coordinates[:, 0]) * 1e9, np.unique(coordinates[:, 1]) * 1e9
-X , Y = np.linspace(np.min(x), np.max(x), 3000), np.linspace(np.min(y), np.max(y), 200)
+X, Y = np.linspace(np.min(x), np.max(x), 3000), np.linspace(np.min(y), np.max(y), 200)
 X, Y = np.meshgrid(X, Y)
 # x, y = x.T, y.T
 
@@ -66,6 +70,7 @@ data = plot_i_state(0)
 mx = scipy.interpolate.griddata((data0[:, 0] * 1e9, data0[:, 1] * 1e9),
                                 data[:, 3], (X, Y), method='cubic')
 
+# Set the Mayavi figure
 f = mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(1000, 700))
 f.scene.off_screen_rendering = True
 surf = mlab.surf(X.T, Y.T, mx.T,
@@ -75,14 +80,15 @@ surf = mlab.surf(X.T, Y.T, mx.T,
                  )
 ms = surf.mlab_source
 
-# From mayavi record:
+# Set the camera view using Mayavi record tool:
 f.scene.camera.position = [2325.4326884312218, 931.86343668888458, 481.2859227706723]
 f.scene.camera.focal_point = [1500.0, 100.0, 0.0]
 f.scene.camera.view_angle = 30.0
 f.scene.camera.view_up = [-0.27111041026308819, -0.26616607661021852, 0.92501608910813182]
 f.scene.camera.clipping_range = [3.9937280454826229, 3993.7280454826227]
+# Text object with the time in nanoseconds
 text = mlab.text(0.05, 0.86,
-                 '{:.3f} ns'.format(0 * 1e-3), width=0.15,
+                 '{:.3f} ns'.format(0.5 * 1e-3), width=0.15,
                  line_width=1.,
                  color=(0, 0, 0)
                  )
@@ -90,11 +96,14 @@ text = mlab.text(0.05, 0.86,
 
 def plot_mayavi_snap(i):
 
+    # Interpolate the m_x data into a regular grid so we can plot it using
+    # a surface
     data = plot_i_state(i)
     mx = scipy.interpolate.griddata((data0[:, 0] * 1e9, data0[:, 1] * 1e9),
                                     data[:, 3], (X, Y), method='cubic')
     ms.reset(x=X.T, y=Y.T, scalars=mx.T)
-    text.text = '{:.3f} ns'.format(i * 1e-3)
+    # Put a text with the time
+    text.text = '{:.3f} ns'.format(i * 0.5 * 1e-3)
 
     f.scene.save('animation/' + 'snap_{:06}.png'.format(i))
 
